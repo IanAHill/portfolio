5a4e180eb1a55eae98d7230db652cb92b5185a56
Ian Hill <ianhill@Ians-MBP.home>
2022-08-19 11:21:44 -0400

    added media query for about me

--- src/components/about.js
+++ src/components/about.js
@@ -6,7 +6,7 @@ import "./about.css"
 export const About = () => {
 
     return (
-        <div id="about-container">
+        <div id="about-container" className = "fade-in">
             <img src = "aboutMe.jpeg" id="headshot"/>
             <h4 id="brand-statement">Hi, I'm Ian! I'm a full-stack web developer and former Lutheran pastor with nearly ten years of experience using web technologies to help an organization realize its mission. {<br />}{<br />}I recently earned a certificate in full stack development from Rutgers University, with skills in HTML, CSS, JavaScript, databases, Express, Node, React, and so many more skills. I am known as a team player with strengths in server-side development, databases, and oral and written communication. {<br />}{<br />}In my past work, I successfully rebranded an organization, which increased exposure and social media following. I recently worked on a team of five to develop a cloud-based family cookbook app that helps families preserve favorite recipes. I’m looking forward to applying my lifelong passion for coding to work on a team in a new organization.</h4>
         </div>
--- src/components/welcome.js
+++ src/components/welcome.js
@@ -9,7 +9,7 @@ export const Welcome = () => {
         <div className = "fade-in">
     
         <h1>Full-Stack Web Developer</h1><br />
-        <h3>I'm a full-stack developer and former pastor using web technologies to help organizations realize their mission</h3>
+        <h3>I'm a full-stack developer and former pastor using web technologies to help organizations realize their mission</h3><br />
         <img src = "avataaars.png" alt = "cartoon avatar image" id = "avatar"></img>
         </div>
     )
