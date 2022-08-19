7c613a7b397677a6d40f623c20158bdc0f7597ab
Ian Hill <ianhill@Ians-MBP.home>
2022-08-19 10:15:34 -0400

    added social profile links

--- src/components/footer.js
+++ src/components/footer.js
@@ -6,8 +6,8 @@ export const Footer = () => {
         <div id = "footer">
     
             <p>Made with love by me 2022</p>
-            <img src = "github.png" height="5%" width="5%"/>
-            <img src = "linkedin.png" height="5%" width="5%"/>
+            <a href="https://github.com/IanAHill"><img src = "github.png" height="5%" width="5%"/></a>
+            <a href="https://www.linkedin.com/in/ian-hill-9019911a0/"><img src = "linkedin.png" height="5%" width="5%"/></a>
 
 
         </div>
