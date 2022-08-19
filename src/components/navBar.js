ea92c89b8000eb3a005fd0ab17cfa6d0add05320
Ian Hill <ianhill@Ians-MBP.home>
2022-08-19 12:11:32 -0400

    fixed hamburger menu to close when links clicked

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -3,17 +3,17 @@ import { Section } from "./section";
 import "./navBar.css"
 export const NavBar = () => {
 
-    
+
     const [section, setSection] = useState("Welcome");
 
     const changeSection = (section) => {
         setSection(section);
     }
 
-    
 
-    
-    
+
+
+
     return (
         <div>
 
@@ -22,13 +22,34 @@ export const NavBar = () => {
                 <nav class="navbar">
                     <a href="#welcome" class="nav-branding" onClick={() => changeSection("Welcome")}>Ian Hill</a>
                     <ul class="nav-menu">
-                        <li class="nav-item"><a href="#about" class="nav-link" onClick={() => changeSection("About")}>About Me</a></li>
-                        <li class="nav-item"><a href="#projects" class="nav-link" onClick={() => changeSection("Projects")}>Projects</a></li>
-                        <li class="nav-item"><a href="#contact" class="nav-link" onClick={() => changeSection("Contact")}>Contact</a></li>
+                        <li class="nav-item"><a href="#about" class="nav-link" onClick={() => {
+                            changeSection("About");
+                            const hamburger = document.querySelector(".hamburger");
+                            const navMenu = document.querySelector(".nav-menu");
+                            hamburger.classList.remove("active");
+                            navMenu.classList.remove("active");
+                        }}>About Me</a></li>
+
+                        <li class="nav-item"><a href="#projects" class="nav-link" onClick={() => {
+                            changeSection("Projects");
+                            const hamburger = document.querySelector(".hamburger");
+                            const navMenu = document.querySelector(".nav-menu");
+                            hamburger.classList.remove("active");
+                            navMenu.classList.remove("active");
+                        }}>Projects</a></li>
+
+                        <li class="nav-item"><a href="#contact" class="nav-link" onClick={() => {
+                            changeSection("Contact");
+                            const hamburger = document.querySelector(".hamburger");
+                            const navMenu = document.querySelector(".nav-menu");
+                            hamburger.classList.remove("active");
+                            navMenu.classList.remove("active");
+                        }}>Contact</a></li>
                     </ul>
+
                     <div class="hamburger" onClick={() => {
-                      const hamburger = document.querySelector(".hamburger");
-                      const navMenu = document.querySelector(".nav-menu");
+                        const hamburger = document.querySelector(".hamburger");
+                        const navMenu = document.querySelector(".nav-menu");
                         hamburger.classList.toggle("active");
                         navMenu.classList.toggle("active");
                     }}>
